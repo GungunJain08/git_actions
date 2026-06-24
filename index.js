4b3cff0150e8ac2976a707864c8b0ec3ee7df898
Mayra <gungunjn08@gmail.com>
2026-06-24 22:58:46 +0530

    feat: initialize Node.js express server with Docker and container orchestration support

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+    res.json({ msg: "Hello Github actions v1" })
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on the port no ${PORT}`)
+})
