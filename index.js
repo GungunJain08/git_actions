e751f213eeb9198c5686fd3edf6056eb6a8d8fba
Mayra <gungunjn08@gmail.com>
2026-06-24 23:59:14 +0530

    feat: add deployment workflow via SSH and update root route message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-    res.json({ msg: "Hello Github actions from Mayra_theCutiee" })
+    res.json({ msg: "Hello Github actions from Mayra_theCutiee_" })
 });
 app.listen(PORT, () => {
     console.log(`Server is running on the port no ${PORT}`)
