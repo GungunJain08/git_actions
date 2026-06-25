626e23ec5f71ecb10f81b02d4b6a492f64f66dd3
Mayra <gungunjn08@gmail.com>
2026-06-25 16:24:56 +0530

    feat: update greeting message in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-    res.json({ msg: "Hello Github actions from Mayra_theCutiee_" })
+    res.json({ msg: "Hello Github actions from Mayra_theCutiee_..Patotiee" })
 });
 app.listen(PORT, () => {
     console.log(`Server is running on the port no ${PORT}`)
