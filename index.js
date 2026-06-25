9fde55274dd097e8f7695bb2a2a7ae69453482b6
Mayra <gungunjn08@gmail.com>
2026-06-25 23:08:52 +0530

    feat: add container hostname to response and configure Nginx load balancing for multiple app replicas

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express")
+const os = require("os");
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-    res.json({ msg: "Hello Github actions from Mayra_theCutiee_..Patotieeee." })
+    res.json({
+        msg: "Hello Github actions from Mayra_theCutiee_..Patotieeee.",
+        container_id: os.hostname()
+    })
 });
 app.listen(PORT, () => {
     console.log(`Server is running on the port no ${PORT}`)
